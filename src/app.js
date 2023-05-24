3ac9c0e020ae699a95112cb7597c80d401f3855a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-24 09:26:40 -0500

    feat: sessions

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 import express from 'express'
-import cookieParser from 'cookie-parser'
+// import cookieParser from 'cookie-parser'
+import session from 'express-session'
 
 const app = express()
-app.use(cookieParser('victoriasecret'))
+// app.use(cookieParser('victoriasecret'))
+app.use(session({
+    secret: 'victoriasecret',
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
     //detectar el pais del client
-    res.cookie('location', 'PE', { signed: true }).send('Bienvenido!')
+    // res.cookie('location', 'PE', { signed: true }).send('Bienvenido!')
+    req.session.user = {
+        name: 'Alex',
+        role: 'teacher'
+    }
+    res.send('ok')
 })
 
 app.get('/preference', (req, res) => {
-    res.cookie('preference', 'dark', { signed: true }).json({ message: 'Configuración guardada con éxito!'})
+    // res.cookie('preference', 'dark', { signed: true }).json({ message: 'Configuración guardada con éxito!'})
+    req.session.preference = 'dark'
+    res.send('ok')
 })
 
 app.get('/mode', (req, res) => {
     // res.send(req.cookies)
-    res.send(req.signedCookies)
+    // res.send(req.signedCookies)
+    res.send(req.session.user)
 })
 
 app.get('/logout', (req, res) => {
